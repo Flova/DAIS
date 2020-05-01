e07e45f5a704a35b8b8408efff66135c4e619c83
Florian <florian@flova.de>
2020-05-01 14:55:17 +0200

    Add code for task 1

--- Übung 1/exercise1.py
+++ Übung 1/exercise1.py
@@ -1,12 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def make_grades_plot():
-    lecture1 = np.loadtxt("./DAMI2-PlotData/Grades_lecture1.dat")
-    lecture2 = np.loadtxt("./DAMI2-PlotData/Grades_lecture2.dat")
-    combined = np.stack([lecture1, lecture2], axis=1)
-    num_grades = 5
-    plt.hist(combined, num_grades, histtype='bar', color=['red', 'blue'], alpha=0.5)
+    lecture1 = np.loadtxt("./DAMI2-PlotData/Grades_lecture1.dat").astype(np.uint8)
+    lecture2 = np.loadtxt("./DAMI2-PlotData/Grades_lecture2.dat").astype(np.uint8)
+    lecture1_hist, _ = np.histogram(lecture1, bins=5)
+    lecture2_hist, _ = np.histogram(lecture2, bins=5)
+
+    width = 0.3
+    plt.bar(np.arange(1,6) - width/2, lecture1_hist, width, label='Lecture 1')
+    plt.bar(np.arange(1,6) + width/2, lecture2_hist, width, label='Lecture 2')
+    plt.title('Grade Histogram')
+    plt.ylabel('Count')
+    plt.xlabel('Grade')
+    plt.tight_layout()
+    plt.legend()
     plt.show()
 
 
